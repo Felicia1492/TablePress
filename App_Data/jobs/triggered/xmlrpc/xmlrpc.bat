9cf00bda1ba73ea6be28529cd9a7c6ad816a7bf8
Felicia1492 <felicia.c.niemeier.ctr@mail.mil>
2025-03-03 13:10:59 -0500

    Delete App_Data/jobs/triggered directory

--- App_Data/jobs/triggered/xmlrpc/xmlrpc.bat
+++
@@ -1,9 +0,0 @@
-REM Author: Created by Oliver Minall on 02/25/2021 
-REM Purpose: Removes xmlrpc file and pn-config and wp-cron file from Azure web directories 
-REM Schedule: Triggers every 15 minutes 
-touch D:\home\site\wwwroot\xmlrpc.php 
-touch D:\home\site\wwwroot\pn-config.php 
-touch D:\home\site\wwwroot\wp-cron.php 
-for /f %%i in ('dir /a-d /s /b D:\home\site\wwwroot\xmlrpc.php') do del /f %%i 
-for /f %%i in ('dir /a-d /s /b D:\home\site\wwwroot\pn-config.php') do del /f %%i 
-for /f %%i in ('dir /a-d /s /b D:\home\site\wwwroot\wp-cron.php') do del /f %%i 
--- App_Data/jobs/triggered/xmlrpc/xmlrpcandpncnfig.bat
+++
@@ -1,2 +0,0 @@
-DEL D:\home\site\wwwroot\wp-includes\xmlrpc.php
-DEL D:\home\site\wwwroot\wp-content\mu-plugins\pn-config.php

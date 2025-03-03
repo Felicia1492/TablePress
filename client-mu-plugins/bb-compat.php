10b5e4f8d536822ffaa846bae1d8f6b67e78802c
Felicia1492 <felicia.c.niemeier.ctr@mail.mil>
2025-03-03 13:19:53 -0500

    Delete client-mu-plugins directory

--- client-mu-plugins/bb-compat.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Beaver Builder will attempt to regenerate css/cache files and make HTTP API calls on the filesystem with each origin request causing up to 12s load times.
-// This forces the assets to load inline
-add_filter( 'fl_builder_render_assets_inline', '__return_true' );
